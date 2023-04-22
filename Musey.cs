2e2ecb2b8a729bdf9acd35c2a02a2280b28e04af
gigachad1488 <iluhadolmatov@bk.ru>
2023-04-22 11:18:03 +0700

    Добавьте файлы проекта.

---
+++ Musey.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_Korondenko
+{
+    public class Musey
+    {
+        private string _name;
+        private int _vek;
+        private int _price;
+        private string _type;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Vek
+        {
+            get { return _vek; }
+            set { _vek = value; }
+        }
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+       /* public Musey (string Name, int Carates, int Price, string Type)
+        {
+            _name = Name;
+            _Vek = Vek;
+            _price = Price;
+            _type = Type;
+
+        }
+       */
+    }
+}
---
+++ MuseyBaza.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exam_Korondenko
+{
+    public class MuseyBaza
+
+    {
+        public List<Musey> museyList { get;set; }
+        public void Initialize() => museyList = new List<Musey>
+         {
+         new Musey()
+        {
+            Name = "Меч Македонского",
+            Vek = 12,
+            Price = 100000,
+            Type = "Меч"
+        },
+        new Musey()
+        {
+            Name = "Репак бай илюхич",
+            Vek = 21,
+            Price = 150000,
+            Type = "репак"
+        },
+         };
+     
+        
+    }
+}
+
+    
